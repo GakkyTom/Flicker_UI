a44d0966eecfbdcdb0d66260bf13207138dbcf1a
GakkyTom <itagaki.tomoya@eversense.co.jp>
2020-04-20 21:53:33 +0900

    Initial Commit

---
+++ tinderMock/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tinderMock
+//
+//  Created by 板垣智也 on 2020/04/20.
+//  Copyright © 2020 板垣智也. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
